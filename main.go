0bc0674db7371b28eef4c5417eeaa0762b381ed3
diabeticMike <tmymichael@gmail.com>
2022-01-18 12:06:46 +0200

    check jenkins webhook trigger

--- main.go
+++ main.go
@@ -5,14 +5,13 @@ import (
 	"net/http"
 )
 
-
-func main(){
+func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`Hello World`))
+		w.Write([]byte(`Hello World 2`))
 	}).Methods(http.MethodGet)
 	println("listen to 8080")
-	if err := http.ListenAndServe(":8080", router); err != nil{
+	if err := http.ListenAndServe(":8080", router); err != nil {
 		panic(err)
 	}
 }
